c986ed82ccab8e5c4b68f962c459ca2997acc2e4
MaugeaLee <dlqhgur@naver.com>
2025-07-20 02:11:15 +0900

    update(api_scan): except에서 return 되어 큐가 refresh되지 않은 문제 해결

--- API/SCAN/api_scan.py
+++ API/SCAN/api_scan.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 import asyncio
 import json
+import time
 from API.CONFIG.bogger import BoggerDevLogger
 from API.CONFIG.static import request_scan_topic, response_scan_topic, iot_response_queue
 
@@ -14,23 +15,31 @@ async def scan_device_status(request: Request):
         topic = request_scan_topic,
         message="1"
     )
+
     # -- Queue를 이용한 비동기 데이터 상태 관리 --
     # 디바이스 응답을 비동기로 기다림
     timeout = 1  # 응답 대기 시간 (초)
+    time.sleep(timeout) # 큐에 값이 들어오길 기다리기
+
+    response = {}
     queuing_list = []
     while not iot_response_queue.empty():
         try:
             response_payload_str = iot_response_queue.get_nowait()
-            response_payload_json = json.dumps(response_payload_str)
+            response_payload_json = json.loads(response_payload_str)
+
+            # 큐 내부 값을 리스트로 저장하기
             queuing_list.append(response_payload_json)
-            logger.info(response_payload_str)
+            # 이거 지금 queue의 요소 중 하나라도 json 형식을 어기면 이전 큐까지 모두 Except되는데 이거 해결해야함
         except asyncio.TimeoutError:
             logger.error(f"MQTT on_message timeout: {timeout}초 내에 IoT 디바이스로부터 응답을 받지 못했습니다.")
-            return {'error': 'Timeout waiting for device response'}, 504
+            response = {'error': 'Timeout waiting for device response'}
         except json.JSONDecodeError:
             logger.error(f"MQTT on_message return error: 수신된 응답이 유효한 JSON 형식이 아닙니다.")
-            return {'error': 'Invalid JSON response from device'}, 500
+            response =  {'error': f'Invalid JSON response from device {iot_response_queue}'}
         except Exception as e:
             logger.error(f'MQTT on_message error: 예상치 못한 오류 발생: {e}')
-            return {'error': f'An unexpected error occurred: {str(e)}'}, 500
-    return {'ok': queuing_list}
+            response =  {'error': f'An unexpected error occurred: {str(e)}'}
+
+    response = {'ok': queuing_list}
+    return response
