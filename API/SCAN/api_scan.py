6ecee464c550408a4b05036a905a392b95724dc2
MaugeaLee <dlqhgur@naver.com>
2025-07-19 19:06:42 +0900

    update(api_scan.py): 큐 내부의 모든 값을 load 해서 저장함

--- API/SCAN/api_scan.py
+++ API/SCAN/api_scan.py
@@ -16,21 +16,21 @@ async def scan_device_status(request: Request):
     )
     # -- Queue를 이용한 비동기 데이터 상태 관리 --
     # 디바이스 응답을 비동기로 기다림
-    try:
-        timeout = 1 # 응답 대기 시간 (초)
-        response_payload_str = await asyncio.wait_for(iot_response_queue.get(), timeout=timeout) # response_quere에서 메세지를 비동기적을 ㅗ가져옴
-        logger.info(response_payload_str)
-        print(response_payload_str)
-        return json.dumps(response_payload_str)
-
-    except asyncio.TimeoutError:
-        logger.error(f"MQTT on_message timeout: {timeout}초 내에 IoT 디바이스로부터 응답을 받지 못했습니다.")
-        return {'error': 'Timeout waiting for device response'}, 504
-    except json.JSONDecodeError:
-        logger.error(f"MQTT on_message return error: 수신된 응답이 유효한 JSON 형식이 아닙니다.")
-        return {'error': 'Invalid JSON response from device'}, 500
-    except Exception as e:
-        logger.error(f'MQTT on_message error: 예상치 못한 오류 발생: {e}')
-        return {'error', f'An unexpected error occurred: {str(e)}'}, 500
-
-
+    timeout = 1  # 응답 대기 시간 (초)
+    queuing_list = []
+    while not iot_response_queue.empty():
+        try:
+            response_payload_str = iot_response_queue.get_nowait()
+            response_payload_json = json.dumps(response_payload_str)
+            queuing_list.append(response_payload_json)
+            logger.info(response_payload_str)
+        except asyncio.TimeoutError:
+            logger.error(f"MQTT on_message timeout: {timeout}초 내에 IoT 디바이스로부터 응답을 받지 못했습니다.")
+            return {'error': 'Timeout waiting for device response'}, 504
+        except json.JSONDecodeError:
+            logger.error(f"MQTT on_message return error: 수신된 응답이 유효한 JSON 형식이 아닙니다.")
+            return {'error': 'Invalid JSON response from device'}, 500
+        except Exception as e:
+            logger.error(f'MQTT on_message error: 예상치 못한 오류 발생: {e}')
+            return {'error': f'An unexpected error occurred: {str(e)}'}, 500
+    return {'ok': queuing_list}
